a177c76245f129463cbadba257f6ca3c69ccf585
Volkan <volkan.swf@gmail.com>
2026-05-06 00:16:14 +0200

    add required functions and test benchmark

---
+++ setup.sh
@@ -0,0 +1,20 @@
+set -e
+
+wget https://rwth-aachen.sciebo.de/s/RapAoed1dxG1PMs/download -O large_models.zip
+unzip large_models.zip -d large_models
+
+echo "Moving large benchmark files"
+cd large_models/vnncomp2024/
+for d in *
+do
+    cd $d/seed_896832480/;
+    mkdir -p ../../../../benchmarks/$d/onnx
+    mkdir -p ../../../../benchmarks/$d/vnnlib
+    find . -type f -exec mv "{}" "../../../../benchmarks/$d/{}" \;
+    cd ../../;
+done
+cd ../..
+rm -r large_models large_models.zip
+
+echo "Unzipping"
+gunzip -r benchmarks/
