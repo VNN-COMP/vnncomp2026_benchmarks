889c98d2758a85e1035bbb74cdbe85dfdf2017bf
Volkan <volkan.swf@gmail.com>
2026-06-18 23:15:05 +0200

    fix safenlp path prefix adding

--- run_all_categories.sh
+++ run_all_categories.sh
@@ -188,11 +188,11 @@ do
 
         mkdir -p ${COUNTEREXAMPLES_FOLDER}/${CATEGORY}
         COUNTEREXAMPLE_FILE=${COUNTEREXAMPLES_FOLDER}/${CATEGORY}/${ONNX_FILENAME}_${VNNLIB_FILENAME}.counterexample
-        # If the benchmark is the safenlp benchmark and the vnnlib file contains "ruarobot", prepend ruarobot_ to the filname
-        if [[ $CATEGORY == "safenlp" && $VNNLIB_PATH == *"ruarobot"* ]]; then
+        # If the benchmark is a safenlp benchmark and the vnnlib file contains "ruarobot", prepend ruarobot_ to the filname
+        if [[ $CATEGORY == "safenlp" || $CATEGORY == safenlp_* ]] && [[ $VNNLIB_PATH == *"ruarobot"* ]]; then
             COUNTEREXAMPLE_FILE=${COUNTEREXAMPLES_FOLDER}/${CATEGORY}/ruarobot_${ONNX_FILENAME}_${VNNLIB_FILENAME}.counterexample
         fi
-        if [[ $CATEGORY == "safenlp" && $VNNLIB_PATH == *"medical"* ]]; then
+        if [[ $CATEGORY == "safenlp" || $CATEGORY == safenlp_* ]] && [[ $VNNLIB_PATH == *"medical"* ]]; then
             COUNTEREXAMPLE_FILE=${COUNTEREXAMPLES_FOLDER}/${CATEGORY}/medical_${ONNX_FILENAME}_${VNNLIB_FILENAME}.counterexample
         fi
         $SCRIPT_PATH/run_single_instance.sh v1 "$TOOL_FOLDER" "$CATEGORY" "$ONNX_PATH" "$VNNLIB_PATH" "$TIMEOUT" "$RESULT_CSV_FILE" "${COUNTEREXAMPLE_FILE}"
